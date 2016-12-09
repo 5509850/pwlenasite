44b30ee0d6eb63d047358cba0995c87f99db57f6
Aliaksandr_Harbunou <Aliaksandr_Harbunou@epam.com>
2016-12-09 14:47:29 +0300

    add work with bot and webhook

--- lenapw.test/Controllers/WebHookController.cs
+++ lenapw.test/Controllers/WebHookController.cs
@@ -4,6 +4,15 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Web.Http;
 
+
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Telegram.Bot.Types;
+using File = System.IO.File;
+using Telegram.Bot.Types.Enums;
+
 namespace lenapw.test.Controllers
 {
     public class WebHookController : ApiController
@@ -21,6 +30,49 @@ namespace lenapw.test.Controllers
                            new Client { Id = 6, Title = Environment.Version.Revision.ToString()},
                            new Client { Id = 7, Title = Environment.Version.ToString()}
                        };
+        }
+
+        public ICollection<Client> Get(int id)
+        {
+            //http://localhost:18442/api/webhook/5
+            //http://localhost:18442/api/webhook/get/5
+
+           
+            {
+                return new Collection<Client>
+                       {
+                            {
+                                new Client { Id = id, Title = "wrong" }
+                            }
+                       };
+            }   
+        }
+
+        public async Task<IHttpActionResult> Post(Update update)
+        {
+            var message = update.Message;          
+
+            if (message.Type == MessageType.TextMessage)
+            {
+                // Echo each Message
+               Message x = await Bot.Api.SendTextMessage(message.Chat.Id, message.Text);
+            }
+            else if (message.Type == MessageType.PhotoMessage)
+            {
+                // Download Photo
+                var file = await Bot.Api.GetFile(message.Photo.LastOrDefault()?.FileId);
+
+                var filename = file.FileId + "." + file.FilePath.Split('.').Last();
+
+                using (var saveImageStream = File.Open(filename, FileMode.Create))
+                {
+                    await file.FileStream.CopyToAsync(saveImageStream);
+                }
+
+                Message x = await Bot.Api.SendTextMessage(message.Chat.Id, "Thx for the Pics");
+            }
+
+            return Ok();
         }
     }
 }
---
+++ lenapw.test/Models/Bot.cs
@@ -0,0 +1,9 @@
+﻿using Telegram.Bot;
+
+namespace lenapw.test.Models
+{
+    static public class Bot
+    {
+        public static readonly TelegramBotClient Api = new TelegramBotClient("<You Bot Token>");
+    }
+}
